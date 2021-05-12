d36c6df32a4ab037abf9728e98ab0875edfe607c
unknown <letrananhkiet@canvas-works.asia>
2021-05-12 15:38:08 +0700

    cm

--- index.js
+++ index.js
@@ -7,50 +7,16 @@ const axios = require('axios');
 const qs = require('qs');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const User = require('./models/user.model');
-const nodemailer = require("nodemailer");
-
-async function main(mail) {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
-  
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
-      },
-    });
-  
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: mail, // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-  
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
   
 
 /* Set up static file */
 app.use(express.static('public'));
 /* Set up bodyparser */
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const connectDB = require("./db");
 // Connect to database
@@ -78,6 +44,24 @@ app.get('/',async (req, res) => {
     } else {
         res.redirect('/login');
     }
+});
+
+app.post('/mess', async (req,res) => {
+    const { mess } = req.body;
+
+    const { access_token, id } = req.cookies;
+
+    const result = await axios({
+        method: "GET",
+        url: "https://api.line.me/v2/bot/info",
+        headers: {
+            "Authorization": "Bearer " + "wgaasdt9U9g/4avEsttQhFkaPlORxEKYAFK5bNoW4PtNzCK2PzdzKTV5r1MbmJtKTX4aL2gMgaaDt6R22B4FwUrwv0ZV9xxJBL2+KyOAIGJguUbCK8l0z+TjjEtVMicmW2TyRB8ryLfJKe7inPh3igdB04t89/1O/w1cDnyilFU="
+        }
+    });
+
+    console.log("result", result.data);
+
+
 })
 
 app.get('/login', (req, res) => {
@@ -147,7 +131,7 @@ app.get('/callback', async (req, res) => {
                 res.send("Error save User");
             }
         });
-        await main(userInfo1.data.email).catch(console.error);
+
     } else {
         await User.findOneAndUpdate({lineId: userId}, {
             name: displayName,
